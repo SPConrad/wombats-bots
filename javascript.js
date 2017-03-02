1b57e7033c20aa5df110879e3317ee6e601ae878
C.P. Dehli <dehli@users.noreply.github.com>
2017-03-02 15:27:50 -0500

    Created turning javascript bot.

---
+++ javascript.js
@@ -0,0 +1,11 @@
+(state, time_left) => ({
+    command: {
+        action: "turn",
+        metadata: {
+            direction: "right"
+        }
+    },
+    state: {
+        hello: "world"
+    }
+});
