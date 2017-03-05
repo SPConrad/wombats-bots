f1a5dcbf150a8089da40f4f6c775085989095e26
C.P. Dehli <dehli@users.noreply.github.com>
2017-03-05 16:58:33 -0500

    Update python.py

--- python.py
+++ python.py
@@ -1,2 +1,12 @@
-def wombat:
-    pass
+def wombat(state, time_left):
+    return {
+        'command': {
+            'action': 'turn',
+            'metadata': {
+                'direction': 'right'
+            }
+        },
+        'state': {
+            'hello': 'world'
+        }
+    }
